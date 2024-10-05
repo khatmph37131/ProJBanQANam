85143b46404564e151a92fbcca046d7632093628
khatmph37131 <khatmph37131@fpt.edu.vn>
2024-10-05 17:25:06 +0700

    demo DATN

---
+++ src/main/java/com/example/projbanqanam/ProJBanQaNamApplication.java
@@ -0,0 +1,13 @@
+package com.example.projbanqanam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProJBanQaNamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProJBanQaNamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projbanqanam/ProJBanQaNamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projbanqanam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProJBanQaNamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
