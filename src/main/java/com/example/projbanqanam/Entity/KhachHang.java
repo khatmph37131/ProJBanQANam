18e3e99678e93e3083b2d5392f985027667138f4
khatmph37131 <khatmph37131@fpt.edu.vn>
2024-10-17 22:49:59 +0700

    DATN up 1

--- src/main/java/com/example/projbanqanam/Entity/KhachHang.java
+++ src/main/java/com/example/projbanqanam/Entity/KhachHang.java
@@ -1,4 +1,52 @@
 package com.example.projbanqanam.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "KHACHHANG")
 public class KhachHang {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "IDKHACHHANG")
+    private Integer id;
+
+    @Column(name = "MAKH")
+    private String makh;
+
+    @Column(name = "TENKH")
+    private String ten;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "NGAYSINH")
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    private Date ngaysinh;
+
+    @Column(name = "GIOITINH")
+    private String gioitinh;
+
+    @Column(name = "SDT")
+    private String sdt;
+
+    @Column(name = "DIACHI")
+    private String diachi;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "MATKHAU")
+    private String matkhau;
+
+    @Column(name = "TRANGTHAI")
+    private String tt;
 }
--- src/main/java/com/example/projbanqanam/Repo/KhachHangRepo.java
+++ src/main/java/com/example/projbanqanam/Repo/KhachHangRepo.java
@@ -1,4 +1,16 @@
 package com.example.projbanqanam.Repo;
 
-public interface KhachHangRepo {
-}
+import com.example.projbanqanam.Entity.KhachHang;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface KhachHangRepo extends JpaRepository<KhachHang, Integer> {
+    // -- Tìm kiếm --
+    List<KhachHang> findByMakhContaining(String makh);
+    // Check trùng mã khách hàng
+    boolean existsByMakh(String makh);
+    }
+
